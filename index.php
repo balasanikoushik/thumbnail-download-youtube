0b536847c25e50e94dcadb502e8abf5241e9ce8d
balasanikoushik <koushikbalasani187@gmail.com>
2024-01-05 14:45:33 +0530

    changed background

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 require 'index.html';
 if (isset($_POST['button'])) {
-  $imgUrl = $_POST['imgurl'];
+  $imgUrl = filter_var($_POST['imgurl'], FILTER_SANITIZE_URL);
+  if (filter_var($imgUrl, FILTER_VALIDATE_URL) === false) {
+    die('Not a valid URL');
+  }
   $ch = curl_init($imgUrl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   $downloadImg = curl_exec($ch);
+  if (curl_errno($ch)) {
+    die('Error: ' . curl_error($ch));
+  }
+  $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
   curl_close($ch);
-  header('Content-type: image/jpg');
+  header('Content-type: ' . $contentType);
   header('Content-Disposition: attachment;filename="thumbnail.jpg"');
   echo $downloadImg;
 }
